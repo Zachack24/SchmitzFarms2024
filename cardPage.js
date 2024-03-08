60153ecf71a0fdaabfa4cca837ca76f93ee8192c
Zach Schmitz <zach24@iastate.edu>
2024-03-08 05:58:16 -0600

    Woo Hoo Baby

---
+++ cardPage.js
@@ -0,0 +1,71 @@
+// Zach Schmitz
+// zach24@iastate.edu
+// 3/5/2024
+
+
+let cardNumber = localStorage.getItem('cardNumber');
+
+fetch("./data.json")
+.then(response => response.json())
+.then(data => loadCardPage(data,cardNumber));
+
+function loadCardPage(data,i) {
+    console.log("Card number set to:", i);
+    // Find the element “col” in HTML
+    var CardSheep = document.getElementById("cardpage");
+    // Read every Sheep from the array
+   
+    let card = "card" + i.toString();
+
+    let name = data.sheep[i].name;
+    let birth_year = data.sheep[i].birth_year;
+    let father = data.sheep[i].father;
+    let mother = data.sheep[i].mother; 
+    let breed = data.sheep[i].breed;
+    let weight = data.sheep[i].weight;
+    let price = data.sheep[i].price;
+    let gender = data.sheep[i].gender;
+    let image = data.sheep[i].image;
+
+    let AddCardSheep = document.createElement("div");
+
+    // create Bootstrap card
+    AddCardSheep.innerHTML = ` 
+
+    <div id=${card} >
+    <div class="d-flex justify-content-between align-items-center">
+        <div class="card-body">
+            <div class="d-flex align-items-center" style="margin-bottom: .5vw;">
+                <a class="ret-link" href="./index.html">Home</a>
+                <a>&nbsp | &nbsp</a>
+                <a class="ret-link" href="./sheep.html">Sheep</a>
+                <a>&nbsp | &nbsp</a>
+                <a>${name}</a>
+            </div>
+            <img id="card-image" src= ${image}></img>
+        </div>
+        <div class="card-body">
+            <h1 class="name-text"> <strong>${name}</strong></h1>
+            <div class="main-text">
+                <h5 class="card-text"> ${gender} ${birth_year} | ${father} X ${mother}</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h5 class="card-text"> Breed: ${breed} | Weight: ${weight}</h5>
+                <div class="text-left" style="margin-top: 3vw;">
+                <button type="button" class="btn btn-lg btn-outline-secondary" onclick="openPurchase(${price}, '${name}')">$${price}.00</button>
+
+                </div>
+                
+            </div>   
+        </div>
+    </div>
+</div>
+    `;
+    // append new division
+    CardSheep.appendChild(AddCardSheep);
+   } // end of Function
+
+   function openPurchase(price,name){
+        var message = `You have selected: ${name}\nPrice: $${price}.00`;
+        window.alert(message);
+
+   }
--- sheep.js
+++ sheep.js
@@ -4,31 +4,31 @@
 
 fetch("./data.json")
 .then(response => response.json())
-.then(mySheep => loadSheep(mySheep));
+.then(data => loadSheep(data));
 
-function loadSheep(mySheep) {
+function loadSheep(data) {
 
  // Find the element “col” in HTML
  var CardSheep = document.getElementById("col");
 
- var checkboxes = [];
+
  var cards = [];
 
  // Read every Sheep from the array
- for (var i = 0; i<mySheep.sheep.length; i++){
+ for (var i = 0; i<data.sheep.length; i++){
 
  
      let card = "card" + i.toString();
 
-     let name = mySheep.sheep[i].name;
-     let birth_year = mySheep.sheep[i].birth_year;
-     let father = mySheep.sheep[i].father;
-     let mother = mySheep.sheep[i].mother; 
-     let breed = mySheep.sheep[i].breed;
-     let weight = mySheep.sheep[i].weight;
-     let price = mySheep.sheep[i].price;
-     let gender = mySheep.sheep[i].gender;
-     let image = mySheep.sheep[i].image;
+     let name = data.sheep[i].name;
+     let birth_year = data.sheep[i].birth_year;
+     let father = data.sheep[i].father;
+     let mother = data.sheep[i].mother; 
+     let breed = data.sheep[i].breed;
+     let weight = data.sheep[i].weight;
+     let price = data.sheep[i].price;
+     let gender = data.sheep[i].gender;
+     let image = data.sheep[i].image;
 
      let AddCardSheep = document.createElement("div");
 
@@ -42,10 +42,10 @@ function loadSheep(mySheep) {
          <img src=${image} class="card-img-top" alt="..."></img>
          <div class="card-body">
              <p class="card-text"> <strong>${name}</strong></p>
-             <p> ${gender} ${birth_year} | ${father} X ${mother}</p>
-             <p> Breed: ${breed} Weight: ${weight}</p>
+             <p class="card-text"> ${gender} ${birth_year} | ${father} X ${mother}</p>
+             <p class="card-text"> Breed: ${breed} Weight: ${weight}</p>
                  <div class="d-flex justify-content-between align-items-center"> 
-                    <button type="button" class="btn btn-sm btn-outline-secondary">More Info</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary" onclick="openCardPage(${i})">More Info</button>
                     <small class="text-body-secondary">$${price}.00</small>
                 </div>
          </div>
@@ -64,7 +64,12 @@ function loadSheep(mySheep) {
    
 } // end of Function
 
+function openCardPage(cardNumber){
 
+
+    localStorage.setItem('cardNumber',cardNumber);
+    window.location.href='cardPage.html';
+}
 
 
 
