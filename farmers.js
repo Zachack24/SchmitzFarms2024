2c3a7923ab1716fad8767dddb548871225d40166
Blake Clabaugh <clab22@iastate.edu>
2024-03-08 23:16:45 -0600

    we are so done

--- farmers.js
+++ farmers.js
@@ -1,9 +1,6 @@
-
-
-
-
-
-
+//Blake Clabaugh
+//clab22@iastate.edu
+//3/8/2024
 
 fetch("./data.json")
     .then(response => response.json())
@@ -12,20 +9,48 @@ fetch("./data.json")
 
 
 function getFamily(data) {
-    var add = document.getElementById("myCarousel");
-    var cards = [];
+    var Sadd = document.getElementById("i");
+    let name = data.farmers[0].name;
+    let image = data.farmers[0].image;
+    let description = data.farmers[0].description;
+    let AddFam = document.createElement("div");
+    AddFam.classList.add("carousel-inner")
+    AddFam.innerHTML=`
 
-    for (var i = 1; i < data.farmers.length; i++) {
-        let card = "card" + i.toString();
-        let name = data.farmers[i].name;
-        let age = data.farmers[i].age;
-        let description = data.farmers[i].description;
+    <div class="carousel-item active">
+        <img src=${image} width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></img>
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>${name}</h1>
+            <p>${description}</p>
+          </div>
+        </div>
+      </div>`;
+    Sadd.appendChild(AddFam);
+
+    for(let i = 1;i < data.farmers.length;i++) {
         let AddFam = document.createElement("div");
-        AddFam.classList.add("carousel-item active");
-        AddCard.innerHTML = `
-            
-        `;
+        AddFam.classList.add("carousel-inner")
+        name = data.farmers[i].name;
+        image = data.farmers[i].image;
+        description = data.farmers[i].description;
+
+
+        AddFam.innerHTML = ` 
+ 
+        <div class="carousel-item">
+        <img src=${image} width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="60%" fill="var(--bs-secondary-color)"/></img>
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>${name}</h1>
+            <p>${description}</p>
+          </div>
+        </div>
+      </div>`;
+
+        Sadd.appendChild(AddFam);
+    }
 
     }
-}
+
 
